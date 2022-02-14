b9a7f6f7d7da59a86d18ea9b807b71d707d04ed8
Rebecca <rebecca@heyman.com>
2022-02-14 00:36:16 -0600

    remove CRA readme

---
+++ src/components/banner.js
@@ -0,0 +1,14 @@
+import './banner.css'
+
+function Banner() {
+  return(
+    <div class="banner">
+      <div class="backgroundBox">
+        <p id="message1">Rebecca Heyman</p>
+        <p id="message2">Software Developer</p>
+      </div>
+    </div>
+  )
+}
+
+export default Banner;
