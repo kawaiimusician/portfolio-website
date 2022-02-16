cec49e47f895cfd1908bdfe146bd3b3c989e1b00
Rebecca <rebecca@heyman.com>
2022-02-16 15:55:07 -0600

    bio section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Banner from './components/banner';
+import Bio from './components/bio';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <Bio />
     </div>
   );
 }
---
+++ src/components/bio.js
@@ -0,0 +1,15 @@
+import "./bio.css"
+
+function Bio() {
+  return(
+    <div class="contain row">
+      <img src="images/rebecca-pic.jpg" alt="Rebecca Heyman" class="profilePic col-md-3 d-block mx-auto mx-lg-0"></img>
+      <div class="aboutSection col-md-7 d-block mx-auto mx-md-0">
+        <h1>About Rebecca</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti temporibus quis debitis? Laboriosam dolorem incidunt repellendus unde natus atque similique quaerat perspiciatis, magni pariatur iure totam quas quibusdam dignissimos est!</p>
+      </div>
+    </div>
+  )
+}
+
+export default Bio;
